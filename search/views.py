99c0ef5366fa77661b395c9425e3196380e56841
Choonghee Lee <choonghee.dev@gmail.com>
2020-07-08 21:19:05 +0900

    sending data to another page

--- search/views.py
+++ search/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_page(request):
+    return render(
+        request,
+        'search/index.html'
+    )
+
+
+def search_page(request):
+    term = request.GET.get('term', 'google')
+    return render(
+        request,
+        'search/search.html',
+        {'term': term}
+    )
