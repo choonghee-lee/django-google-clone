14f16c556c5c8aa15de73e006eb339bab2461b27
Choonghee Lee <choonghee.dev@gmail.com>
2020-07-09 14:24:55 +0900

    site search

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from .parser import (
     create_link,
     refine_links,
 )
+from .models import Site, Image
 
 
 def index_page(request):
@@ -17,10 +18,15 @@ def search_page(request):
     term = request.GET.get('term', 'google')
     searchType = request.GET.get('type', 'sites')
 
-    url = "http://www.bbc.com"
-    parser = DomDocumentParser()
-    links = parser.get_links(url)
-    refined_links = refine_links(url, links)
+    # url = "http://www.bbc.com"
+    # parser = DomDocumentParser()
+    # links = parser.get_links(url)
+    # refined_links = refine_links(url, links)
+
+    links = Site.objects.filter(title__contains=term) | Site.objects.filter(
+        url__contains=term) | Site.objects.filter(description__contains=term) | Site.objects.filter(keywords__contains=term)
+    links = links.order_by('-clicks')
+    links_count = links.count()
 
     return render(
         request,
@@ -28,6 +34,7 @@ def search_page(request):
         {
             'term': term,
             'type': searchType,
-            'links': refined_links
+            'links': links,
+            'links_count': links_count,
         }
     )
